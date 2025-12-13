2b5012207c7ca341a13abfeb1940b3f6f65f0b4c
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-13 11:46:02 +0700

    feat: define routes for customer menu and order placement

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+use App\Http\Controllers\CustomerMenuController;
+use App\Http\Controllers\OrderController;
+
+Route::get('/menu/{tableId}', [CustomerMenuController::class, 'index'])->name('customer-menu.index');
+Route::post('/order', [OrderController::class, 'placeOrder'])->name('order.place');
