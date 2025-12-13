f5fdbdf634cda64004836bbb4ad22b425204131c
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-13 11:46:21 +0700

    feat: add navigation component for table display

---
+++ resources/js/pages/menu/partials/nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="py-1 flex items-center justify-between bg-primary px-4">
+        <img class="h-14 w-14 rounded-full max-[400px]:h-12 max-[400px]:w-12" src="/images/logo.svg " alt="Logo" />
+        <p class="text-2xl font-semibold text-white">Bàn {{ props.tableNumber }}</p>
+    </nav>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    tableNumber: string;
+}
+
+const props = defineProps<Props>();
+</script>
