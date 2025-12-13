6e272915322590552b0810b9229f52e56eecac5e
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-13 11:07:28 +0700

    feat: add Category and Menu interfaces with related types

---
+++ resources/js/types/menu.ts
@@ -0,0 +1,22 @@
+export interface Menu {
+    id: number;
+    name: string;
+    foods: Food[];
+}
+
+export interface Food {
+    id: number;
+    name: string;
+    price: number;
+    note: string | null;
+    dishes: Dish[];
+}
+
+export interface Dish {
+    id: number;
+    name: string;
+    additional_price: number;
+    note: string | null;
+    cooking_method_id: number;
+}
+
