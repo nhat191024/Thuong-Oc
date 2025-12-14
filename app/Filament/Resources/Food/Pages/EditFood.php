10491fedb1e868cd52c835e97497a181a49ac23e
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-14 16:52:43 +0700

    feat: add Food resource with CRUD pages and schema configuration

---
+++ app/Filament/Resources/Food/Pages/EditFood.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Filament\Resources\Food\Pages;
+
+use App\Filament\Resources\Food\FoodResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditFood extends EditRecord
+{
+    protected static string $resource = FoodResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+            RestoreAction::make(),
+        ];
+    }
+}
