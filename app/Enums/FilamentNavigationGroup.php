7aef12a7a059d6f59b3ca8a2b1e3b882ac835879
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-14 16:55:06 +0700

    feat: add label for FOOD enum in FilamentNavigationGroup

--- app/Enums/FilamentNavigationGroup.php
+++ app/Enums/FilamentNavigationGroup.php
@@ -6,10 +6,12 @@ use Filament\Support\Contracts\HasLabel;
 
 enum FilamentNavigationGroup implements HasLabel
 {
+    case FOOD;
 
     public function getLabel(): string
     {
         return match ($this) {
+            FilamentNavigationGroup::FOOD => __('Thực phẩm'),
         };
     }
 }
