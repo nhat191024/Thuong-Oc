091a25ce57496f3555cfd91c5d31f78f0be7977b
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-19 15:34:46 +0700

    feat: add Branch resource with create, edit, and list functionalities

---
+++ app/Filament/Resources/Branches/BranchResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources\Branches;
+
+use App\Models\Branch;
+
+use BackedEnum;
+
+use App\Filament\Resources\Branches\Pages\CreateBranch;
+use App\Filament\Resources\Branches\Pages\EditBranch;
+use App\Filament\Resources\Branches\Pages\ListBranches;
+use App\Filament\Resources\Branches\Schemas\BranchForm;
+use App\Filament\Resources\Branches\Tables\BranchesTable;
+
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BranchResource extends Resource
+{
+    protected static ?string $model = Branch::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedBuildingStorefront;
+
+    public static function getModelLabel(): string
+    {
+        return __('Chi nhánh');
+    }
+
+    public static function form(Schema $schema): Schema
+    {
+        return BranchForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return BranchesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListBranches::route('/'),
+            // 'create' => CreateBranch::route('/create'),
+            // 'edit' => EditBranch::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getRecordRouteBindingEloquentQuery(): Builder
+    {
+        return parent::getRecordRouteBindingEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Branches/Tables/BranchesTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\Branches\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+
+class BranchesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('Tên chi nhánh'))
+                    ->searchable(),
+                TextColumn::make('deleted_at')
+                    ->label(__('Đã xóa lúc'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('created_at')
+                    ->label(__('Tạo lúc'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->label(__('Cập nhật lúc'))
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
