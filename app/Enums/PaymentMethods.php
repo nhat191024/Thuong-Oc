7373b7a51efed10340f423abbea0338b5417903b
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-20 20:39:30 +0700

    feat: add label method to PaymentMethods enum for improved localization

--- app/Enums/PaymentMethods.php
+++ app/Enums/PaymentMethods.php
@@ -6,4 +6,12 @@ enum PaymentMethods: string
 {
     case CASH = 'cash';
     case QR_CODE = 'qr_code';
+
+    public function label(): string
+    {
+        return match ($this) {
+            PaymentMethods::CASH => __('Tiền mặt'),
+            PaymentMethods::QR_CODE => __('QR Code'),
+        };
+    }
 }
