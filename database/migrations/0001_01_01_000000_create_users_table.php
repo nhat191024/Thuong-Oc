d7d960f7bea73e1a775e0cfcfa18a426833705c3
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-20 14:43:50 +0700

    feat: make branch_id nullable and indexed in users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('branch_id');
+            $table->unsignedBigInteger('branch_id')->nullable()->index();
             $table->string('username')->unique();
             $table->string('password');
             $table->rememberToken();
