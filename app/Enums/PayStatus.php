044c08807cb134f4ddbde2410f76ff4d785fde17
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-20 20:34:46 +0700

    feat: add label method to PayStatus enum for better status representation

--- app/Enums/PayStatus.php
+++ app/Enums/PayStatus.php
@@ -7,4 +7,13 @@ enum PayStatus: string
     case PAID = 'paid';
     case UNPAID = 'unpaid';
     case CANCELLED = 'cancelled';
+
+    public function label(): string
+    {
+        return match($this) {
+            PayStatus::PAID => __('Đã thanh toán'),
+            PayStatus::UNPAID => __('Chưa thanh toán'),
+            PayStatus::CANCELLED => __('Đã huỷ'),
+        };
+    }
 }
