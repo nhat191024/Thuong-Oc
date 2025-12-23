cf530f833fc1658e4378a43ee6b8d4994e5a236a
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-23 19:08:41 +0700

    fix: update logo path in nav component for consistency

--- resources/js/pages/menu/partials/nav.vue
+++ resources/js/pages/menu/partials/nav.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="py-1 flex items-center justify-between bg-primary px-4">
-        <img class="h-14 w-14 rounded-full max-[400px]:h-12 max-[400px]:w-12" src="/images/logo.svg " alt="Logo" />
+        <img class="h-14 w-14 rounded-full max-[400px]:h-12 max-[400px]:w-12" src="/logo.svg " alt="Logo" />
         <p class="text-2xl font-semibold text-white">Bàn {{ props.tableNumber }}</p>
     </nav>
 </template>
