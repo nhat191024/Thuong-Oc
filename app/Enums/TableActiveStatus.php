a1a804046f890b5042926a5fbe67a85d39bf5e50
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-23 21:53:02 +0700

    feat: add label method to TableActiveStatus enum for better localization

--- app/Enums/TableActiveStatus.php
+++ app/Enums/TableActiveStatus.php
@@ -6,4 +6,12 @@ enum TableActiveStatus: string
 {
     case ACTIVE = 'active';
     case INACTIVE = 'inactive';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::ACTIVE => __('Hoạt Động'),
+            self::INACTIVE => __('Không Hoạt Động'),
+        };
+    }
 }
