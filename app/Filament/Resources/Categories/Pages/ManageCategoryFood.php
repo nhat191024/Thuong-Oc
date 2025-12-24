65efe1188e9412adf8201df47fe1845d8907f8ae
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-24 23:46:10 +0700

    feat: implement cache clearing on table reorder in ManageCategoryFood

--- app/Filament/Resources/Categories/Pages/ManageCategoryFood.php
+++ app/Filament/Resources/Categories/Pages/ManageCategoryFood.php
@@ -5,10 +5,15 @@ namespace App\Filament\Resources\Categories\Pages;
 use App\Filament\Resources\Categories\CategoryResource;
 use App\Filament\Resources\Categories\Resources\Food\FoodResource;
 
+use App\Enums\CacheKeys;
+
 use Filament\Tables\Table;
 use Filament\Actions\CreateAction;
 
 use Filament\Resources\Pages\ManageRelatedRecords;
+
+use Illuminate\Support\Facades\Cache;
+
 
 class ManageCategoryFood extends ManageRelatedRecords
 {
@@ -42,5 +47,13 @@ class ManageCategoryFood extends ManageRelatedRecords
     public function table(Table $table): Table
     {
         return $table;
+    }
+
+    public function reorderTable(array $order, string|int|null $draggedRecordKey = null): void
+    {
+        parent::reorderTable($order);
+
+        Cache::forget(CacheKeys::MENUS->value);
+        Cache::forget(CacheKeys::MENU_CATEGORIES->value);
     }
 }
