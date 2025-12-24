77ab4f9320aed0ce560efb0e6d5d33b7327104f0
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-24 21:23:03 +0700

    fix: update logo source to use dynamic app logo and change tableNumber type to number

--- resources/js/pages/menu/partials/nav.vue
+++ resources/js/pages/menu/partials/nav.vue
@@ -1,13 +1,20 @@
 <template>
-    <nav class="py-1 flex items-center justify-between bg-primary px-4">
-        <img class="h-14 w-14 rounded-full max-[400px]:h-12 max-[400px]:w-12" src="/logo.svg " alt="Logo" />
+    <nav class="flex items-center justify-between bg-primary px-4 py-1">
+        <img class="h-14 w-14 rounded-full max-[400px]:h-12 max-[400px]:w-12" :src="settings.app_logo ?? ''" alt="Logo" />
         <p class="text-2xl font-semibold text-white">Bàn {{ props.tableNumber }}</p>
     </nav>
 </template>
 
 <script setup lang="ts">
+import { AppSettings } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const page = usePage();
+const settings = computed(() => page.props.app_settings as AppSettings);
+
 interface Props {
-    tableNumber: string;
+    tableNumber: number;
 }
 
 const props = defineProps<Props>();
