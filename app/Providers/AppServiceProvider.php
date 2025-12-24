2c8f96ceda202fa2da25ac4e8830de718b7bb3ea
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-24 21:23:25 +0700

    feat: implement application settings sharing in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Inertia\Inertia;
+
+use App\Settings\AppSettings;
+
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +24,38 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        try {
+            $settings = app(AppSettings::class);
+            $settingsArray = $this->getSettingsArray($settings);
+        } catch (\Exception $e) {
+            $settingsArray = [
+                'app_name'    => config('app.name'),
+                'app_logo'    => '/logo.svg',
+                'app_favicon' => '/favicon.ico',
+            ];
+        }
+
+        Inertia::share([
+            'app_settings'  => fn() => $settingsArray
+        ]);
+
+        View::share('settings', $settingsArray);
+    }
+
+    private function getSettingsArray(AppSettings $settings)
+    {
+        try {
+            return [
+                'app_name'    => $settings->app_name,
+                'app_logo'    => secure_asset($settings->app_logo),
+                'app_favicon' => secure_asset($settings->app_favicon),
+            ];
+        } catch (\Exception $e) {
+            return [
+                'app_name'    => config('app.name'),
+                'app_logo'    => null,
+                'app_favicon' => null,
+            ];
+        }
     }
 }
