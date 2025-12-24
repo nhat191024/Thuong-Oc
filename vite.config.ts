f90651e7f79688f368cf6396d793d0d7473d9524
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-24 21:15:48 +0700

    fix: remove server configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,6 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        cors: true,
-        allowedHosts: ['thuongoc.test'],
-        hmr: {
-            host: 'thuongoc.test',
-            protocol: 'wss',
-        },
-    },
     plugins: [
         laravel({
             input: [
