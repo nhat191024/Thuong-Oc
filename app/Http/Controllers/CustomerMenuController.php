bae88b7dd934e14f5a98caac762487ded8351eb0
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-26 01:41:54 +0700

    feat: refactor CustomerMenuController to utilize MenuService for menu retrieval

--- app/Http/Controllers/CustomerMenuController.php
+++ app/Http/Controllers/CustomerMenuController.php
@@ -9,13 +9,21 @@ use App\Enums\CacheKeys;
 use App\Enums\TableActiveStatus;
 use App\Enums\PayStatus;
 
+use App\Services\MenuService;
+
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Support\Facades\Cache;
 
-
 class CustomerMenuController extends Controller
 {
+    protected MenuService $menuService;
+
+    public function __construct()
+    {
+        $this->menuService = app(MenuService::class);
+    }
+
     /**
      * Display the customer menu for the specified table.
      */
@@ -29,46 +37,7 @@ class CustomerMenuController extends Controller
             'bill.billDetails.dish.cookingMethod',
         ])->findOrFail($tableId);
 
-        $menus = Cache::rememberForever(CacheKeys::MENUS->value, function () {
-            $categories = Category::select(['id', 'name'])
-                ->has('food.dishes')
-                ->with([
-                    'food' => function ($query) {
-                        $query->select(['id', 'category_id', 'name', 'price', 'discount_price', 'is_favorite', 'note', 'sold_count'])
-                            ->has('dishes')
-                            ->with([
-                                'media',
-                                'dishes:id,food_id,cooking_method_id,additional_price,note',
-                                'dishes.cookingMethod:id,name',
-                            ])
-                            ->orderBy('order');
-                    },
-                ])
-                ->orderBy('order')
-                ->get();
-
-            return $categories->map(fn($category) => [
-                'id' => $category->id,
-                'name' => $category->name,
-                'foods' => $category->food->map(fn($food) => [
-                    'id' => $food->id,
-                    'name' => $food->name,
-                    'is_favorite' => $food->is_favorite,
-                    'is_discounted' => $food->discount_price > 0,
-                    'price' => $food->price,
-                    'discount_price' => $food->discount_price,
-                    'note' => $food->note,
-                    'image' => $food->getFirstMediaUrl('default', 'preview'),
-                    'sold_count' => $food->sold_count,
-                    'dishes' => $food->dishes->map(fn($dish) => [
-                        'id' => $dish->id,
-                        'name' => $dish->cookingMethod?->name,
-                        'additional_price' => $dish->additional_price,
-                        'note' => $dish->note,
-                    ]),
-                ]),
-            ]);
-        });
+        $menus = $this->menuService->getMenus();
 
         $categories = Cache::remember(CacheKeys::MENU_CATEGORIES->value, 3600, function () {
             return Category::select(['id', 'name'])
