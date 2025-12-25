7b73b2fc396e9d1d1d4bbd08889bab103bbe33d9
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-26 01:11:23 +0700

    feat: create auth routes for user login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+include __DIR__ . '/api/auth.php';
