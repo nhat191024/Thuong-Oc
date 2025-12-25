70911891d49c9c69200d067407bca0a386c5c7a7
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-26 02:09:45 +0700

    feat: refactor getMenus method to return CategoryResourceCollection

--- app/Services/MenuService.php
+++ app/Services/MenuService.php
@@ -6,6 +6,8 @@ use App\Enums\CacheKeys;
 
 use App\Models\Category;
 
+use App\Http\Resources\CategoryResourceCollection;
+
 use Illuminate\Support\Facades\Cache;
 
 class MenuService
@@ -30,27 +32,7 @@ class MenuService
                 ->orderBy('order')
                 ->get();
 
-            return $categories->map(fn($category) => [
-                'id' => $category->id,
-                'name' => $category->name,
-                'foods' => $category->food->map(fn($food) => [
-                    'id' => $food->id,
-                    'name' => $food->name,
-                    'is_favorite' => $food->is_favorite,
-                    'is_discounted' => $food->discount_price > 0,
-                    'price' => $food->price,
-                    'discount_price' => $food->discount_price,
-                    'note' => $food->note,
-                    'image' => $food->getFirstMediaUrl('default', 'preview'),
-                    'sold_count' => $food->sold_count,
-                    'dishes' => $food->dishes->map(fn($dish) => [
-                        'id' => $dish->id,
-                        'name' => $dish->cookingMethod?->name,
-                        'additional_price' => $dish->additional_price,
-                        'note' => $dish->note,
-                    ]),
-                ]),
-            ]);
+            return new CategoryResourceCollection($categories);
         });
 
         return $menus;
