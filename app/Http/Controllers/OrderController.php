b223680be75166165e8f923c998d358b979d43a7
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-01 02:50:27 +0700

    refactor: optimize placeOrder method for better readability and performance

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -22,34 +22,15 @@ class OrderController extends Controller
      */
     public function placeOrder(PlaceOrderRequest $request)
     {
-        $table = Table::find($request->input('table_id'))
-            ->with([
-                'bill' => function ($query) {
-                    $query->where('pay_status', PayStatus::UNPAID);
-                }
-            ])->first();
+        $table = Table::with([
+            'bill' => function ($query) {
+                $query->where('pay_status', PayStatus::UNPAID);
+            }
+        ])->find($request->input('table_id'));
 
-        $createdEntities = 0;
-        $billTotal = 0;
         if ($table->is_active === TableActiveStatus::ACTIVE) {
             //If table is occupied update current bill with new order
             $bill = $table->bill;
-            $billTotal = $bill->total;
-            foreach ($request->input('dishes') as $dish) {
-                $billDetail = BillDetail::create([
-                    'bill_id' => $bill->id,
-                    'dish_id' => $dish['dish_id'],
-                    'quantity' => $dish['quantity'],
-                    'price' => $dish['price'],
-                    'note' => $dish['note'],
-                ]);
-                $createdEntities++;
-                $billTotal += $dish['price'] * $dish['quantity'];
-
-                //TODO: add event new dish ordered to notify kitchen
-            }
-            $bill->total = $billTotal;
-            $bill->save();
         } else {
             //If table is free, create new bill and add order
             $table->is_active = TableActiveStatus::ACTIVE;
@@ -60,23 +41,36 @@ class OrderController extends Controller
                 'branch_id' => $request->branch_id,
                 'user_id' => $request->user_id,
             ]);
-            $bill->save();
-            foreach ($request->dishes as $dish) {
-                $billDetail = BillDetail::create([
+        }
+
+        $billTotal = $bill->total ?? 0;
+
+        foreach ($request->input('dishes') as $dish) {
+            $existingDetail = BillDetail::where('bill_id', $bill->id)
+                ->where('dish_id', $dish['dish_id'])
+                ->where('note', $dish['note'])
+                ->first();
+
+            if ($existingDetail) {
+                $existingDetail->quantity += $dish['quantity'];
+                $existingDetail->save();
+            } else {
+                BillDetail::create([
                     'bill_id' => $bill->id,
                     'dish_id' => $dish['dish_id'],
                     'quantity' => $dish['quantity'],
                     'price' => $dish['price'],
                     'note' => $dish['note'],
                 ]);
-                $billTotal += $dish['price'] * $dish['quantity'];
-                $createdEntities++;
-
-                //TODO: add event new dish ordered to notify kitchen
             }
-            $bill->total = $billTotal;
-            $bill->save();
+
+            $billTotal += $dish['price'] * $dish['quantity'];
+
+            //TODO: add event new dish ordered to notify kitchen
         }
+
+        $bill->total = $billTotal;
+        $bill->save();
 
         return redirect()->back()->with('success', 'Order placed successfully');
     }
