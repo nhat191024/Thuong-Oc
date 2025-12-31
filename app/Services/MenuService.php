b00dc17ae5ffb1c88baec96cd1596d52f593d78d
Taiyo Furuhashi <richberchannel01@gmail.com>
2025-12-31 20:04:40 +0700

    feat: enhance getMenus method to support array and collection return types

--- app/Services/MenuService.php
+++ app/Services/MenuService.php
@@ -12,9 +12,15 @@ use Illuminate\Support\Facades\Cache;
 
 class MenuService
 {
-    public function getMenus() : CategoryResourceCollection
+    /**
+     * Get menus with categories and foods
+     * @var bool $useCollection Whether to return as a resource collection
+     *
+     * @return CategoryResourceCollection|mixed
+     */
+    public function getMenus(bool $useCollection)
     {
-        $menus = Cache::rememberForever(CacheKeys::MENUS->value, function () {
+        $menus = Cache::rememberForever(CacheKeys::MENUS->value . '-' . ($useCollection ? 'collection' : 'array'), function () use ($useCollection) {
             $categories = Category::select(['id', 'name'])
                 ->has('food.dishes')
                 ->with([
@@ -32,9 +38,46 @@ class MenuService
                 ->orderBy('order')
                 ->get();
 
-            return new CategoryResourceCollection($categories);
+            if ($useCollection) {
+                return new CategoryResourceCollection($categories);
+            } else {
+                return $this->transformMenusToArray($categories);
+            }
         });
 
         return $menus;
+    }
+
+    /**
+     * Transform menus to array
+     *
+     * @param mixed $categories
+     * @return array
+     */
+    private function transformMenusToArray($categories)
+    {
+        return $categories->map(
+            fn($category) => [
+                'id' => $category->id,
+                'name' => $category->name,
+                'foods' => $category->food->map(fn($food) => [
+                    'id' => $food->id,
+                    'name' => $food->name,
+                    'is_favorite' => $food->is_favorite,
+                    'is_discounted' => $food->discount_price > 0,
+                    'price' => $food->price,
+                    'discount_price' => $food->discount_price,
+                    'note' => $food->note,
+                    'image' => $food->getFirstMediaUrl('default', 'preview'),
+                    'sold_count' => $food->sold_count,
+                    'dishes' => $food->dishes->map(fn($dish) => [
+                        'id' => $dish->id,
+                        'name' => $dish->cookingMethod?->name,
+                        'additional_price' => $dish->additional_price,
+                        'note' => $dish->note,
+                    ]),
+                ]),
+            ]
+        );
     }
 }
