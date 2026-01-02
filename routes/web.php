39a439c0709693ef311522c04f06ae521b46b869
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-03 02:04:46 +0700

    feat: add staff routes for authentication and table management

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,19 @@ use App\Http\Controllers\CustomerMenuController;
 use App\Http\Controllers\OrderController;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StaffController;
 
 Route::get('/menu/{tableId}', [CustomerMenuController::class, 'index'])->name('customer-menu.index');
 Route::post('/order', [OrderController::class, 'placeOrder'])->name('order.place');
 
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::prefix('staff')->name('staff.')->group(function () {
+        Route::get('/', [StaffController::class, 'index'])->name('tables');
+        Route::get('/table/{tableId}', [StaffController::class, 'showTable'])->name('table.show');
+    });
+});
