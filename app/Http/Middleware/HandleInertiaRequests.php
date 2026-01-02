c327ae0668bf4a60ace79928895114f2d6145468
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-03 02:05:02 +0700

    feat: add Vietnamese application name to shared props in Inertia middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -41,6 +41,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
+            'name_vi' => config('app.name_vi'),
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
