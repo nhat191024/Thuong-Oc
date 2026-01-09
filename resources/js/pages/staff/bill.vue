02a81837fa26a26a10625d4cbfd721a8814ed962
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-10 02:42:20 +0700

    fix: enhance error message display with timeout functionality

--- resources/js/pages/staff/bill.vue
+++ resources/js/pages/staff/bill.vue
@@ -18,7 +18,7 @@
                 </svg>
                 <span>{{ page.props.flash.success }}</span>
             </div>
-            <div v-if="page.props.flash.error" role="alert" class="mb-4 alert w-full alert-error">
+            <div v-if="page.props.flash.error && showError" role="alert" class="mb-4 alert w-full alert-error">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
                     <path
                         stroke-linecap="round"
@@ -208,6 +208,23 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+
+const showError = ref(false);
+let errorTimeout: ReturnType<typeof setTimeout>;
+
+watch(
+    () => page.props.flash.error,
+    (val) => {
+        if (val) {
+            showError.value = true;
+            clearTimeout(errorTimeout);
+            errorTimeout = setTimeout(() => {
+                showError.value = false;
+            }, 5000);
+        }
+    },
+    { immediate: true },
+);
 
 const discountCode = ref('');
 const isApplyingDiscount = ref(false);
