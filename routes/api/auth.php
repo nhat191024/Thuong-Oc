252d9ae0798ddaf02890d65246345e59ab358f8b
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-01-18 00:44:06 +0700

    feat: add checkToken route to validate API token

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/check-token', [AuthController::class, 'checkToken'])->middleware('auth:sanctum');
