178be6f9fc6c04e1c804523a8c58b7fbb7f41201
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-06-10 18:45:02 +0700

    refactor: update time_in and time_out formatting to use toDateTimeString

--- app/Http/Resources/BillHistoryResource.php
+++ app/Http/Resources/BillHistoryResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class BillHistoryResource extends JsonResource
 {
@@ -16,8 +17,8 @@ class BillHistoryResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'time_in' => $this->time_in ? \Carbon\Carbon::parse($this->time_in)->toDateString() : null,
-            'time_out' => $this->time_out ? \Carbon\Carbon::parse($this->time_out)->toDateString() : null,
+            'time_in' => $this->time_in ? Carbon::parse($this->time_in)->toDateTimeString() : null,
+            'time_out' => $this->time_out ? Carbon::parse($this->time_out)->toDateTimeString() : null,
             'final_total' => $this->final_total,
             'payment_method' => $this->payment_method,
             'pay_status' => $this->pay_status,
