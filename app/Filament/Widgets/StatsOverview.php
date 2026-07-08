fad3c923bc453e63b0519633004207236b0f2ec4
Taiyo Furuhashi <richberchannel01@gmail.com>
2026-07-08 17:34:10 +0700

    feat: change widget active table to today revenue

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -15,19 +15,29 @@ class StatsOverview extends StatsOverviewWidget
 
     protected function getStats(): array
     {
-        return Cache::remember('stats_overview_widget', now()->addDay(), function () {
+        return Cache::remember('stats_overview_widget_' . today()->toDateString(), now()->addMinutes(10), function () {
             return [
-                Stat::make(__('Tổng doanh thu'), number_format(Bill::where('pay_status', PayStatus::PAID)->sum('final_total')) . ' VND')
+                Stat::make(
+                    __('Tổng doanh thu'),
+                    number_format(Bill::where('pay_status', PayStatus::PAID)->sum('final_total')) . ' VND',
+                )
                     ->description(__('Doanh thu tất cả các thời điểm'))
                     ->descriptionIcon('heroicon-m-currency-dollar')
                     ->color('success'),
                 Stat::make(__('Đơn hàng hôm nay'), Bill::whereDate('created_at', today())->count())
                     ->description(__('Số hóa đơn tạo trong ngày'))
                     ->descriptionIcon('heroicon-m-shopping-bag'),
-                Stat::make(__('Bàn đang hoạt động'), Bill::where('pay_status', PayStatus::UNPAID)->count())
-                    ->description(__('Các hóa đơn chưa thanh toán'))
-                    ->descriptionIcon('heroicon-m-user-group')
-                    ->color('warning'),
+                Stat::make(
+                    __('Doanh thu hôm nay'),
+                    number_format(
+                        Bill::where('pay_status', PayStatus::PAID)
+                            ->whereDate('created_at', today())
+                            ->sum('final_total'),
+                    ) . ' VND',
+                )
+                    ->description(__('Doanh thu đã thanh toán trong ngày'))
+                    ->descriptionIcon('heroicon-m-banknotes')
+                    ->color('success'),
                 Stat::make(__('Tổng số khách hàng'), Customer::count())
                     ->description(__('Tổng số khách hàng đã đăng ký'))
                     ->descriptionIcon('heroicon-m-users'),
